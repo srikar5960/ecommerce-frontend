ceac7a9ebb0732b5b29880232e818ec9f3b2cdba
srikar5960 <vijaysrikarpentapati@gmail.com>
2025-08-09 18:50:12 +0530

    port changed again

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -34,7 +34,7 @@ const ProductPage = ({ category }) => {
         {products.length > 0 ? (
           products.map((product) => (
             <div key={product.id} className="product-card">
-              <img src={`http://localhost:8081/api/products/images/${product.imagePath}`} alt={product.name} />
+              <img src={`http://localhost:9090/api/products/images/${product.imagePath}`} alt={product.name} />
               <h3>{product.name}</h3>
               <p>Price: ${product.price}</p>
               <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
